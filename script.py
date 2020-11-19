de88ab607eece063ad9ca20eb594e4478d43d9f4
B082850-2020 <email_not_provided>
2020-11-19 00:21:34 +0000

    interactive input added, yes or no question added

--- script.py
+++ script.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 import os
 import sys
+import subprocess,shutil
+import re
 
-this is the first version of assignment 2 script
+details={}
+details["Protein"]   = input("What protein do you want? ")
+details["Taxon"]    = input("What taxon for the protein? ")
+
+
+def search(protein,taxon) :
+    import string
+    if len(protein)==0:
+        print("no protein was chosen")
+    elif len(taxon)==0:
+        print("no taxon was chosen, please try again..")
+    else:
+        print("\nYou have provided the following keywords for sequence searching:		\n\tProtein:",protein,"\n\tTaxon:",taxon)
+
+search(*list(details.values()))
+    
+        query = input("do you want to continue? y or n")
+        if query == "n":
+            exit()
+        elif query != "y":
+            print("please enter yes or no") 
+print ("\n\nImported os\n\n")
+
+
