aaef26145adda2d17491d60aca8d8f00ab2ef385
B082850-2020 <email_not_provided>
2020-11-19 18:00:22 +0000

    sequence number retrive limit of 1000 set, error traps set

--- script.py
+++ script.py
@@ -18,20 +18,53 @@ def search(protein,taxon,partial) :
         	print("no taxon was chosen, please try again..")
 	elif partial=='y':
 		print("\n Protein sequences searching for:\n\tProtein:",protein,"\n\tTaxon:",taxon,"\n\tPartial: Yes")
-		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " \" " \
-"|efetch -db protein -format fasta >"+ ''.join(i for i in taxon if i.isalnum()) +".nuc.fa"
-		print("This is what I am going to run for you in a shell\n" + es)
-		subprocess.call(es,shell=True)
+		file_name = ''.join(i for i in taxon if i.isalnum())
+		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " \" "
+		es_number = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " \" "+ \
+        	"|grep -i \"count\"|awk \'{split($0,a,\"<|>\");print a[3];}\'"
+		print("This is what I am going to run for you \n" + es)
+		seq_number = subprocess.check_output(es_number,shell=True)
+		if int(seq_number) > 1000:
+			print("\n ** Warning: Over 1000 sequences found, continue is not recommanded, please narrow down your search,"+ \
+			"\n otherwise very slow processing speed and probably taking too much space! Thank you! \n")
+			quit()
+		if int(seq_number) == 0:
+			print("\n Sorry, no sequence was found! Likely spelling mistakes. Please try again. Thank you! \n")
+		else:
+			ef = es + "|efetch -db protein -format fasta >"+ file_name +".nuc.fa"
+			subprocess.call(ef,shell=True)
+			file_contents = open(file_name + ".nuc.fa").read()
+			seq = file_contents.count('>')
+			print ("\n "+str(seq) +" protein sequences successfully retrived! Protein sequences are saved in " \
+			+ file_name +".nuc.fa \n" )
+
 	elif partial=='n':
 		print("\n Protein sequences searching for:\n\tProtein:",protein,"\n\tTaxon:",taxon,"\n\tNo partial")
-		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " Not partial"+ " \" " + \
-"|efetch -db protein -format fasta >"+ ''.join(i for i in taxon if i.isalnum()) +".nuc.fa"
+		file_name = ''.join(i for i in taxon if i.isalnum())
+		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + "Not partial" + " \" " 
+		es_number = "esearch -db protein -query \" "+ taxon +" AND "+ protein + "Not partial" + " \" "+ \
+        	"|grep -i \"count\"|awk \'{split($0,a,\"<|>\");print a[3];}\'"
 		print("This is what I am going to run for you in a shell\n" + es)
 		subprocess.call(es,shell=True)
+		seq_number = subprocess.check_output(es_number,shell=True)
+		if int(seq_number) > 1000:
+			print("\n ** Warning: Over 1000 sequences found, continue is not recommanded, please narrow down your search,"+ \
+			"\n otherwise very slow processing speed and probably taking too much space! Thank you! \n")
+			quit()
+		if int(seq_number) == 0:
+			print("\n Sorry, no sequence was found! Likely spelling mistakes. Please try again. Thank you! \n")
+		else:
+			ef = es + "|efetch -db protein -format fasta >"+ file_name +".nuc.fa"
+			subprocess.call(ef,shell=True)
+			file_contents = open(file_name + ".nuc.fa").read()
+			seq = file_contents.count('>')
+			print ("\n "+str(seq) +" protein sequences successfully retrived! Protein sequences are saved in " \
+			+ file_name +".nuc.fa \n" )
 	else :
 		print("please answer y or n")
+
 search(*list(details.values()))
-    
+ 
 
 
 
