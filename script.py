03ea10d279405dd2d0cedfa5a57b7577b1d663d0
B082850-2020 <email_not_provided>
2020-11-19 14:11:56 +0000

    users now can choose if they want to include partial proteins or not

--- script.py
+++ script.py
@@ -7,21 +7,29 @@ import re
 details={}
 details["Protein"]   = input("What protein do you want? ")
 details["Taxon"]    = input("What taxon for the protein? ")
+details["Partial"] = input ("Do you want to include partial proteins? y or n ")
 
 
-def search(protein,taxon) :
+def search(protein,taxon,partial) :
 	import string
 	if len(protein)==0:
-        	print("no protein was chosen")
+        	print("no protein was chosen,please try again..")
 	elif len(taxon)==0:
         	print("no taxon was chosen, please try again..")
-	else:
-        	print("\nYou have provided the following keywords for sequence searching:		\n\tProtein:",protein,"\n\tTaxon:",taxon)
-	es = "esearch -db protein -query \" "+ taxon +" AND "+ protein +" \" " + \
-	"|efetch -db protein -format fasta >"+ ''.join(i for i in taxon if i.isalnum()) +".nuc.fa"
-	print("This is what I am going to run for you in a shell\n" + es)
-	subprocess.call(es,shell=True)
-
+	elif partial=='y':
+		print("\n Protein sequences searching for:\n\tProtein:",protein,"\n\tTaxon:",taxon,"\n\tPartial: Yes")
+		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " \" " \
+"|efetch -db protein -format fasta >"+ ''.join(i for i in taxon if i.isalnum()) +".nuc.fa"
+		print("This is what I am going to run for you in a shell\n" + es)
+		subprocess.call(es,shell=True)
+	elif partial=='n':
+		print("\n Protein sequences searching for:\n\tProtein:",protein,"\n\tTaxon:",taxon,"\n\tNo partial")
+		es = "esearch -db protein -query \" "+ taxon +" AND "+ protein + " Not partial"+ " \" " + \
+"|efetch -db protein -format fasta >"+ ''.join(i for i in taxon if i.isalnum()) +".nuc.fa"
+		print("This is what I am going to run for you in a shell\n" + es)
+		subprocess.call(es,shell=True)
+	else :
+		print("please answer y or n")
 search(*list(details.values()))
     
 
